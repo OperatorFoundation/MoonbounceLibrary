cd64e3fb75e9d5ab525e8dbe5ab112aba1e6aa0a
consuelita <adelita@operatorfoundation.org>
2022-02-15 17:36:17 -0600

    Update MoonbounceLibraryTests.swift

--- Tests/MoonbounceLibraryTests/MoonbounceLibraryTests.swift
+++ Tests/MoonbounceLibraryTests/MoonbounceLibraryTests.swift
@@ -1,18 +1,24 @@
 import XCTest
 @testable import MoonbounceLibrary
 @testable import MoonbounceNetworkExtensionLibrary
+
 import MoonbounceShared
+import Flower
+import Transmission
 import TunnelClientMock
 import TunnelClient
 import TunnelClientMacOS
 import Chord
 import NetworkExtension
 
-final class MoonbounceLibraryTests: XCTestCase {
-    func testMockRead() throws {
+final class MoonbounceLibraryTests: XCTestCase
+{
+    func testMockRead() throws
+    {
         let pongReceived: XCTestExpectation = XCTestExpectation(description: "pong received")
         
         let newPacket = "45000054edfa00004001baf10A000001080808080800335dde64021860f5bcab0009db7808090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637"
+        
         guard let pingPacket = Data(hex: newPacket) else
         {
             XCTFail()
@@ -21,13 +27,13 @@ final class MoonbounceLibraryTests: XCTestCase {
         
         let nsNumber = NSNumber(value: 4)
         let mptp = MoonbouncePacketTunnelProvider()
-        mptp.configuration.serverAddress = "138.197.196.245"
+        mptp.configuration.serverAddress = ""
         guard var tunnelProviderProtocol = mptp.configuration as? TunnelProviderProtocol else {
             return
         }
         
         var map = [String:Any]()
-        map[Keys.replicantConfigKey.rawValue] = "{\"serverIP\" : \"138.197.196.245\",\"port\" : 1234}".data
+        map[Keys.replicantConfigKey.rawValue] = "{\"serverIP\" : \"10.0.0.1\",\"port\" : 1234}".data
         map[Keys.tunnelNameKey.rawValue] = "Moonbounce"
         
         tunnelProviderProtocol.providerConfiguration = map
@@ -36,6 +42,7 @@ final class MoonbounceLibraryTests: XCTestCase {
         mptp.startTunnel
         {
             maybeError in
+            
             print("ready to go!")
             
             if let flow = mptp.packets as? MockPacketTunnelFlow
@@ -51,6 +58,7 @@ final class MoonbounceLibraryTests: XCTestCase {
         
         wait(for: [pongReceived], timeout: 15) // 15 seconds
     }
+    
     func testReplicantSwiftServer()
     {
         guard let transmissionConnection: Transmission.Connection = TransmissionConnection(host: "138.197.196.245", port: 1234) else
