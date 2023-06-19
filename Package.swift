119b8e129646e64d37d6ea562aa5f2bb3d607931
CryptoSax <joshua@operatorfoundation.org>
2023-06-19 17:27:11 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MoonbounceLibrary",
-    platforms: [.macOS(.v12),
+    platforms: [.macOS(.v13),
                 .iOS(.v15)],
     products: [
         .library(
